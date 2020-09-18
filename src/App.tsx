500647e659d77aef6327aaec2d171dca692a3a3c
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-18 15:28:29 -0300

    Componentizing the Content Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import Calendar from './adminlte/components/Calendar';
-import ChartsWithTabs from './adminlte/components/ChartsWithTabs';
-import ContentHeader from './adminlte/components/ContentHeader';
+import ContentWrapper from './adminlte/components/ContentWrapper';
 import ControlSidebar from './adminlte/components/ControlSidebar';
-import DirectChat from './adminlte/components/DirectChat';
 import Footer from './adminlte/components/Footer';
 import MainSidebar from './adminlte/components/MainSidebar';
-import Map from './adminlte/components/Map';
 import Navbar from './adminlte/components/Navbar';
-import SalesGraph from './adminlte/components/SalesGraph';
-import SmallBoxes from './adminlte/components/SmallBoxes';
-import ToDoList from './adminlte/components/ToDoList';
 
 const App: React.FC = () => {
     return (
@@ -24,53 +17,7 @@ const App: React.FC = () => {
             {/* /.main-navbar */}
 
             {/* Content Wrapper. Contains page content */}
-            <div className="content-wrapper">
-                {/* Content Header (Page header) */}
-                <ContentHeader />
-                {/* /.content-header */}
-                {/* Main content */}
-                <section className="content">
-                    <div className="container-fluid">
-                        {/* Small boxes (Stat box) */}
-                        <div className="row">
-                            <SmallBoxes />
-                        </div>
-                        {/* /.row */}
-                        {/* Main row */}
-                        <div className="row">
-                            {/* Left col */}
-                            <section className="col-lg-7 connectedSortable">
-                                {/* Custom tabs (Charts with tabs)*/}
-                                <ChartsWithTabs />
-                                {/* /.card */}
-                                {/* DIRECT CHAT */}
-                                <DirectChat />
-                                {/*/.direct-chat */}
-                                {/* TO DO List */}
-                                <ToDoList />
-                                {/* /.card */}
-                            </section>
-                            {/* /.Left col */}
-                            {/* right col (We are only adding the ID to make the widgets sortable)*/}
-                            <section className="col-lg-5 connectedSortable">
-                                {/* Map card */}
-                                <Map />
-                                {/* /.card */}
-                                {/* solid sales graph */}
-                                <SalesGraph />
-                                {/* /.card */}
-                                {/* Calendar */}
-                                <Calendar />
-                                {/* /.card */}
-                            </section>
-                            {/* right col */}
-                        </div>
-                        {/* /.row (main row) */}
-                    </div>
-                    {/* /.container-fluid */}
-                </section>
-                {/* /.content */}
-            </div>
+            <ContentWrapper />
             {/* /.content-wrapper */}
 
             {/* Footer */}
