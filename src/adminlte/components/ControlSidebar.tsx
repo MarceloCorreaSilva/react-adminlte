39ab0270e2605bbcb5d137eaa9d21e3b5cf85927
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-18 15:04:35 -0300

    Componentizing the static AdminLTE Dashboard

---
+++ src/adminlte/components/ControlSidebar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ControlSidebar: React.FC = () => {
+    return <aside className="control-sidebar control-sidebar-dark"></aside>;
+};
+
+export default ControlSidebar;
