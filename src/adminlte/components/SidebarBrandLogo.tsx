12141b185eacabce554f4ec312d94d1400ab4478
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-09-18 15:23:17 -0300

    Refactoring: Componentizing the Sidebar component

---
+++ src/adminlte/components/SidebarBrandLogo.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SidebarBrandLogo: React.FC = () => {
+    return (
+        <a href="index3.html" className="brand-link">
+            <img
+                src="assets/adminlte/dist/img/AdminLTELogo.png"
+                alt="AdminLTE Logo"
+                className="brand-image img-circle elevation-3"
+                style={{ opacity: '.8' }}
+            />
+            <span className="brand-text font-weight-light">AdminLTE 3</span>
+        </a>
+    );
+};
+
+export default SidebarBrandLogo;
---
+++ src/adminlte/components/SidebarUserPanel.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SidebarUserPanel: React.FC = () => {
+    return (
+        <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div className="image">
+                <img src="assets/adminlte/dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User" />
+            </div>
+            <div className="info">
+                <a href="!#" className="d-block">
+                    Alexander Pierce
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default SidebarUserPanel;
